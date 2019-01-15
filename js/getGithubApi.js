e939c2c80dfb6de93fd7adab999a4a84a8476134
Desi Mandasari <desimandasari1993@gmail.com>
2019-01-15 10:55:29 +0700

    modified stuyle with flexbox and modified getGithubApi

--- js/getGithubApi.js
+++ js/getGithubApi.js
@@ -8,14 +8,14 @@ const getDataGithub = url => {
       data.map(item => {
         githubFollowers.innerHTML += `
 
-               <li>
+               <div>
                     <div class="card" style="width:200px;">
                     <img class="card-img-top" style="width: 200px; height: 200px;" src="${item.avatar_url}" alt="Avatar" />
                     <div class="card-body">
                         <h5 class="card-title">${item.login}</h5>
                         <a href="${item.html_url}" class="btn btn-primary">Go to Profile</a>
                     </div>
-                </li>
+                </div>
             `;
       });
     });
